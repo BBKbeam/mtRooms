12275ed85e4198fc1f80905151e77b1e7eefab2c
An7ar35 <eadavison@protonmail.com>
2017-03-18 07:34:24 +0100

    [db] Interfaces defined

--- src/db/IDatabase.java
+++ src/db/IDatabase.java
@@ -1,4 +1,13 @@
 package bbk_beam.mtRooms.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
 public interface IDatabase {
+    public bool connect( String host, String username, String password );
+    public bool disconnect();
+    public ResultSet queryDB( String query );
 }
--- src/db/ITimeZoneTranslation.java
+++ src/db/ITimeZoneTranslation.java
@@ -1,4 +1,5 @@
 package bbk_beam.mtRooms.db;
 
 public interface ITimeZoneTranslation {
+    public TimeStamp translate( TimeStamp utc );
 }

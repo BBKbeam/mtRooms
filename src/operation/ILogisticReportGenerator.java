80f4529fa7c0571e1a61d0f24ce00ddb51407942
An7ar35 <eadavison@protonmail.com>
2017-03-18 07:38:18 +0100

    [operation] Interfaces defined

--- src/operation/ILogisticReportGenerator.java
+++ src/operation/ILogisticReportGenerator.java
@@ -1,4 +1,6 @@
 package bbk_beam.mtRooms.operation;
 
 public interface ILogisticReportGenerator {
+    public Collection getInfo( Date date );
+    public Collection getInfo( Date fromDate, Date toDate );
 }

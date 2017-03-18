eb3313ce9760a45ad6b21a29463953f1cd3f65b9
An7ar35 <eadavison@protonmail.com>
2017-03-18 07:48:10 +0100

    [db] Interface docs added

--- src/db/IDatabase.java
+++ src/db/IDatabase.java
@@ -7,7 +7,29 @@ import java.sql.Statement;
 import java.sql.ResultSet;
 
 public interface IDatabase {
-    public bool connect( String host, String username, String password );
-    public bool disconnect();
-    public ResultSet queryDB( String query );
+    /**
+     * Connect to a Database
+     *
+     * @param host     DB Host address
+     * @param username Username
+     * @param password Password
+     * @return Success
+     */
+    public bool connect(String host, String username, String password);
+
+    /**
+     * Disconnect from the connected Database
+     *
+     * @return Success
+     * @throws SQLException when attempting to disconnect nothing
+     */
+    public bool disconnect() throws SQLException;
+
+    /**
+     * Query the Database
+     *
+     * @param query Database query string
+     * @return ResultSet of the query
+     */
+    public ResultSet queryDB(String query);
 }
--- src/db/ITimeZoneTranslation.java
+++ src/db/ITimeZoneTranslation.java
@@ -1,5 +1,11 @@
 package bbk_beam.mtRooms.db;
 
 public interface ITimeZoneTranslation {
-    public TimeStamp translate( TimeStamp utc );
+    /**
+     * Translates a UTC timestamp into a local one
+     *
+     * @param utc UTC Time stamp
+     * @return Local Time stamp
+     */
+    public TimeStamp translate(TimeStamp utc);
 }

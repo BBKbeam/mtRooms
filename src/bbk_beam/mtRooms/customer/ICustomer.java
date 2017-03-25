57302ffbb6d1fd94c5ad5394de44bd6ca2e04285
An7ar35 <eadavison@protonmail.com>
2017-03-25 07:39:05 +0100

    [customer] Added save/load Customer method to ICustomerAccountAccess interface + some formatting

--- src/bbk_beam/mtRooms/customer/ICustomer.java
+++ src/bbk_beam/mtRooms/customer/ICustomer.java
@@ -2,25 +2,44 @@ package bbk_beam.mtRooms.customer;
 
 public interface ICustomer {
     String getCustomerID();
+
     Date getCustomerSince();
+
     String getTitle();
+
     String getName();
+
     String getSurname();
+
     String getAddress();
+
     String getPostCode();
+
     String getCity();
+
     String getCounty();
+
     String getCountry();
+
     String getPhone1();
+
     String getPhone2();
+
     String getEmail();
 
-    void setAddress1( String address1 );
-    void setAddress2(String address2 );
-    void setPostCode( String postCode );
-    void setCity( String city );
-    void setCounty( String county );
-    void setPhone1( String phone1 );
-    void setPhone2( String phone2 );
-    void setEmail( String email );
+    void setAddress1(String address1);
+
+    void setAddress2(String address2);
+
+    void setPostCode(String postCode);
+
+    void setCity(String city);
+
+    void setCounty(String county);
+
+    void setPhone1(String phone1);
+
+    void setPhone2(String phone2);
+
+    void setEmail(String email);
 }
--- src/bbk_beam/mtRooms/customer/ICustomerAccountAccess.java
+++ src/bbk_beam/mtRooms/customer/ICustomerAccountAccess.java
@@ -1,20 +1,48 @@
 package bbk_beam.mtRooms.customer;
 
 import javafx.util.Pair;
+
 import java.util.Collection;
 
 public interface ICustomerAccountAccess {
     /**
      * Gets the Customer account details
+     *
      * @param customerID Customer ID
      * @return Customer details
      */
-    public ICustomer getCustomerAccount( String customerID );
+    public ICustomer getCustomerAccount(String customerID);
+
+    /**
+     * Reloads the customer info from the DB
+     *
+     * @param customer Customer container
+     * @return Reloaded Customer container
+     */
+    public ICustomer getCustomerAccount(ICustomer customer);
 
     /**
      * Finds the records for customer from their surname
+     *
      * @param surname Surname of customer
      * @return List of customer IDs and name of customers with the specified surname
      */
-    public Collection<Pair<String,String>> findCustomer( String surname );
+    public Collection<Pair<String, String>> findCustomer(String surname);
+
+    /**
+     * Creates a new customer
+     *
+     * @param title   Title of customer
+     * @param name    Name of customer
+     * @param surname Surname of customer
+     * @return Customer container
+     */
+    public ICustomer createNewCustomer(String title, String name, String surname);
+
+    /**
+     * Saves changes of a Customer container to the database
+     *
+     * @param customer Customer container
+     */
+    public void saveCustomerChangesToDB(ICustomer customer);
 }

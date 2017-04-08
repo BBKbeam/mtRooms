36639287abad9d704bfd4a555fc4044e1eafa2b7
SU <su.bbk.prj@gmail.com>
2017-04-08 10:36:51 +0100

    [package ] reservation method added

--- src/bbk_beam/mtRooms/reservation/IReservationProcess.java
+++ src/bbk_beam/mtRooms/reservation/IReservationProcess.java
@@ -1,5 +1,6 @@
 package bbk_beam.mtRooms.reservation;
 
 public interface IReservationProcess {
-    public boolean addReservation( );
+    public boolean addReservation();
+    public boolean deleteReservation();
 }

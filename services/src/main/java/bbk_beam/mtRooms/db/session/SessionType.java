928bacc8e3a478ff412efa3486bbcf1ea8901324
An7ar35 <eadavison@protonmail.com>
2017-10-18 21:56:14 +0200

    [db.session] Added level value to SessionType enum

--- services/src/main/java/bbk_beam/mtRooms/db/session/SessionType.java
+++ services/src/main/java/bbk_beam/mtRooms/db/session/SessionType.java
@@ -1,6 +1,26 @@
 package bbk_beam.mtRooms.db.session;
 
 public enum SessionType {
-    ADMIN,
-    USER
+    ADMIN(0),
+    USER(1);
+
+    private final int level;
+
+    /**
+     * Constructor
+     *
+     * @param level Access level
+     */
+    SessionType(int level) {
+        this.level = level;
+    }
+
+    /**
+     * Gets the access level
+     *
+     * @return Access level value
+     */
+    public int level() {
+        return this.level;
+    }
 }

5613e9fc1b29d8a796f19ae88da09b2a4f378abc
An7ar35 <eadavison@protonmail.com>
2017-11-17 00:11:02 +0100

    [reservation.dto] Added getter methods and constructor to DiscountDTO

--- services/src/main/java/bbk_beam/mtRooms/reservation/dto/DiscountDTO.java
+++ services/src/main/java/bbk_beam/mtRooms/reservation/dto/DiscountDTO.java
@@ -6,4 +6,54 @@ public class DiscountDTO {
     private Integer category_id;
     private String category_description;
 
+    /**
+     * Constructor
+     *
+     * @param id                   Discount ID
+     * @param rate                 Discount rate percentage
+     * @param category_id          Discount category ID
+     * @param category_description Discount category description
+     */
+    public DiscountDTO(Integer id, Double rate, Integer category_id, String category_description) {
+        this.id = id;
+        this.rate = rate;
+        this.category_id = category_id;
+        this.category_description = category_description;
+    }
+
+    /**
+     * Gets the discount ID
+     *
+     * @return Discount ID
+     */
+    public Integer id() {
+        return id;
+    }
+
+    /**
+     * Gets the discount rate percentage
+     *
+     * @return Percentage discount rate
+     */
+    public Double rate() {
+        return rate;
+    }
+
+    /**
+     * Gets the discount's category ID
+     *
+     * @return Discount category ID
+     */
+    public Integer categoryID() {
+        return category_id;
+    }
+
+    /**
+     * Gets the discount's category description
+     *
+     * @return Discount category description
+     */
+    public String categoryDescription() {
+        return category_description;
+    }
 }

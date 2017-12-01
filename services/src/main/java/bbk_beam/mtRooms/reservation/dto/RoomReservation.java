140f91b6c56e594dd1ce1f87e00f0d55d5dbf1bf
An7ar35 <eadavison@protonmail.com>
2017-12-01 02:00:52 +0100

    [reservation.dto] Added forgotten cancelled flag property to 'RoomReservation'

--- services/src/main/java/bbk_beam/mtRooms/reservation/dto/RoomReservation.java
+++ services/src/main/java/bbk_beam/mtRooms/reservation/dto/RoomReservation.java
@@ -8,26 +8,30 @@ public class RoomReservation {
     private Date timestamp_out;
     private String note;
     private RoomPrice price;
+    private boolean cancelled_flag;
 
     /**
      * Constructor
      *
-     * @param room          Room
-     * @param timestamp_in  Room reservation start timestamp
-     * @param timestamp_out Room reservation end timestamp
-     * @param note          Note
-     * @param roomPrice     Price for the reservation of the room
+     * @param room           Room
+     * @param timestamp_in   Room reservation start timestamp
+     * @param timestamp_out  Room reservation end timestamp
+     * @param note           Note
+     * @param roomPrice      Price for the reservation of the room
+     * @param cancelled_flag Cancelled room reservation flag
      */
     public RoomReservation(Room room,
                            Date timestamp_in,
                            Date timestamp_out,
                            String note,
-                           RoomPrice roomPrice) {
+                           RoomPrice roomPrice,
+                           boolean cancelled_flag) {
         this.room = room;
         this.timestamp_in = timestamp_in;
         this.timestamp_out = timestamp_out;
         this.note = note;
         this.price = roomPrice;
+        this.cancelled_flag = cancelled_flag;
     }
 
     /**
@@ -75,6 +79,16 @@ public class RoomReservation {
         return this.price;
     }
 
+
+    /**
+     * Gets the cancelled status of the reserved room
+     *
+     * @return Cancelled flag
+     */
+    public boolean isCancelled() {
+        return this.cancelled_flag;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -82,6 +96,7 @@ public class RoomReservation {
 
         RoomReservation that = (RoomReservation) o;
 
+        if (cancelled_flag != that.cancelled_flag) return false;
         if (!room.equals(that.room)) return false;
         if (!timestamp_in.equals(that.timestamp_in)) return false;
         if (!timestamp_out.equals(that.timestamp_out)) return false;
@@ -96,6 +111,7 @@ public class RoomReservation {
         result = 31 * result + timestamp_out.hashCode();
         result = 31 * result + note.hashCode();
         result = 31 * result + price.hashCode();
+        result = 31 * result + (cancelled_flag ? 1 : 0);
         return result;
     }
 

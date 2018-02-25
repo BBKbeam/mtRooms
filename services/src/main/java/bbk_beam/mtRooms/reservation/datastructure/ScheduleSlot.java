e4d947bf0ab6243edaf54a2350d45c925f84e120
An7ar35 <eadavison@protonmail.com>
2018-02-25 22:51:23 +0100

    [reservation.datastructure] Made "ScheduleSlot" comparable and added watcher functionality; [gradle] Upgraded EADjLib version to 0.1.8.

--- services/src/main/java/bbk_beam/mtRooms/reservation/datastructure/ScheduleSlot.java
+++ services/src/main/java/bbk_beam/mtRooms/reservation/datastructure/ScheduleSlot.java
@@ -1,11 +1,31 @@
 package bbk_beam.mtRooms.reservation.datastructure;
 
+import bbk_beam.mtRooms.admin.authentication.Token;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Schedule-slot holder class
  */
-class ScheduleSlot {
+class ScheduleSlot implements Comparable<ScheduleSlot> {
     private TimestampUTC start;
     private TimestampUTC end;
+    private List<Token> watchers;
+
+    /**
+     * Constructor
+     *
+     * @param watcher_token Watcher's session ID
+     * @param start         Start timestamp string (UTC)
+     * @param end           End timestamp string (UTC)
+     */
+    public ScheduleSlot(Token watcher_token, String start, String end) {
+        this.start = new TimestampUTC(start);
+        this.end = new TimestampUTC(end);
+        this.watchers = new ArrayList<>();
+        this.watchers.add(watcher_token);
+    }
 
     /**
      * Constructor
@@ -13,9 +33,10 @@ class ScheduleSlot {
      * @param start Start timestamp string (UTC)
      * @param end   End timestamp string (UTC)
      */
-    ScheduleSlot(String start, String end) {
+    public ScheduleSlot(String start, String end) {
         this.start = new TimestampUTC(start);
         this.end = new TimestampUTC(end);
+        this.watchers = new ArrayList<>();
     }
 
     /**
@@ -23,7 +44,7 @@ class ScheduleSlot {
      *
      * @return Start TimestampUTC
      */
-    TimestampUTC start() {
+    public TimestampUTC start() {
         return this.start;
     }
 
@@ -32,7 +53,51 @@ class ScheduleSlot {
      *
      * @return End TimestampUTC
      */
-    TimestampUTC end() {
+    public TimestampUTC end() {
         return this.end;
+    }
+
+    /**
+     * Adds a watcher to this exact schedule slot
+     *
+     * @param watcher_token
+     * @return Watcher list changed status
+     */
+    public boolean addWatcher(Token watcher_token) {
+        return this.watchers.add(watcher_token);
+    }
+
+    /**
+     * Removes a watcher from the watcher list
+     *
+     * @param watcher_token Watcher token to remove
+     * @return Removal status
+     */
+    public boolean removeWatcher(Token watcher_token) {
+        return this.watchers.remove(watcher_token);
+    }
+
+    /**
+     * Gets the number of watchers of this exact schedule slot
+     *
+     * @return Watcher count
+     */
+    public int watcherCount() {
+        return this.watchers.size();
+    }
+
+    /**
+     * Compares the ScheduleSlot against another
+     *
+     * @param that ScheduleSlot to compare against
+     * @return (- 1) when slot ends before the other, (0) when slot overlaps with other, (1) when slot starts after the other
+     */
+    @Override
+    public int compareTo(ScheduleSlot that) {
+        if (this.end().compareTo(that.start()) < 0)
+            return -1;
+        if (this.start().compareTo(that.end()) > 0)
+            return 1;
+        return 0;
     }
 }

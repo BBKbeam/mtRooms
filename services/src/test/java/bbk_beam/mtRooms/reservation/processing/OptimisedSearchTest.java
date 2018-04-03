2c82a7ba7183d5ea7c3526daaba1a0191b2049a2
An7ar35 <eadavison@protonmail.com>
2018-04-03 12:46:10 +0200

    [reservation.processing] Some small debugging on 'OptimisedSearch' + unit test implemented (forgotten unit test file in previous commit).

--- services/src/test/java/bbk_beam/mtRooms/reservation/processing/OptimisedSearchTest.java
+++ services/src/test/java/bbk_beam/mtRooms/reservation/processing/OptimisedSearchTest.java
@@ -56,44 +56,167 @@ public class OptimisedSearchTest {
         Files.deleteIfExists(Paths.get("optimised_search_test.db"));
     }
 
-    @Test
-    public void search() throws Exception {
-        //TODO
-        Assert.assertTrue(false);
-    }
-
+    /**
+     * Searches for any Rooms that match the properties given
+     */
     @Test
     public void search1() throws Exception {
-        //TODO
-        Assert.assertTrue(false);
+        RoomProperty property = new RoomProperty(
+                RoomProperty.Trilean.TRUE, //fixed chairs
+                RoomProperty.Trilean.UNDEFINED,
+                RoomProperty.Trilean.UNDEFINED,
+                RoomProperty.Trilean.UNDEFINED,
+                -1,
+                -1
+        );
+        List<Room> list = this.optimisedSearch.search(this.token, property);
+        Assert.assertEquals(2, list.size());
+        int validator = 0;
+        for (Room room : list) {
+            if (room.id() == 1 && room.floorID() == 1 && room.buildingID() == 1)
+                validator++;
+            if (room.id() == 8 && room.floorID() == 3 && room.buildingID() == 1)
+                validator++;
+        }
+        Assert.assertEquals("Found " + validator + "/2 matches in list.", 2, validator);
     }
 
+    /**
+     * Searches for Rooms that match the properties given in a building
+     */
     @Test
     public void search2() throws Exception {
-        //TODO
-        Assert.assertTrue(false);
+        RoomProperty property = new RoomProperty(
+                RoomProperty.Trilean.UNDEFINED,
+                RoomProperty.Trilean.TRUE, //catering space
+                RoomProperty.Trilean.UNDEFINED,
+                RoomProperty.Trilean.UNDEFINED,
+                -1,
+                -1
+        );
+        List<Room> list = this.optimisedSearch.search(this.token, 1, property);
+        Assert.assertEquals(4, list.size());
+        int validator = 0;
+        for (Room room : list) {
+            if (room.id() == 4 && room.floorID() == 2 && room.buildingID() == 1)
+                validator++;
+            if (room.id() == 5 && room.floorID() == 2 && room.buildingID() == 1)
+                validator++;
+            if (room.id() == 6 && room.floorID() == 2 && room.buildingID() == 1)
+                validator++;
+            if (room.id() == 7 && room.floorID() == 3 && room.buildingID() == 1)
+                validator++;
+        }
+        Assert.assertEquals("Found " + validator + "/4 matches in list.", 4, validator);
     }
 
+    /**
+     * Searches for Rooms that match the properties given in a floor
+     */
     @Test
     public void search3() throws Exception {
-        //TODO
-        Assert.assertTrue(false);
+        RoomProperty property = new RoomProperty(
+                RoomProperty.Trilean.UNDEFINED,
+                RoomProperty.Trilean.UNDEFINED,
+                RoomProperty.Trilean.UNDEFINED,
+                RoomProperty.Trilean.TRUE, //projector
+                -1,
+                -1
+        );
+        List<Room> list = this.optimisedSearch.search(this.token, 1, 1, property);
+        Assert.assertEquals(1, list.size());
+        int validator = 0;
+        for (Room room : list) {
+            if (room.id() == 3 && room.floorID() == 1 && room.buildingID() == 1)
+                validator++;
+        }
+        Assert.assertEquals("Found " + validator + "/1 matches in list.", 1, validator);
     }
 
+    /**
+     * Searches for available times for a Room
+     */
     @Test
     public void search4() throws Exception {
-        //TODO
-        Assert.assertTrue(false);
+        Room room = new Room(5, 2, 1, 3);
+        Date start1 = TimestampConverter.getDateObject("2018-03-10 09:00:00");
+        Date end1 = TimestampConverter.getDateObject("2018-03-10 21:00:00"); //24x30mns
+        List<TimeSpan> free_slots = this.optimisedSearch.search(this.token, room, start1, end1);
+
+        Assert.assertEquals(10, free_slots.size()); //24-(2+1+1+4+2)
     }
 
+    /**
+     * Searches for available rooms in a building within a time frame
+     */
     @Test
     public void search5() throws Exception {
-        //TODO
-        Assert.assertTrue(false);
+        RoomProperty property = new RoomProperty(
+                RoomProperty.Trilean.UNDEFINED,
+                RoomProperty.Trilean.UNDEFINED,
+                RoomProperty.Trilean.UNDEFINED,
+                RoomProperty.Trilean.UNDEFINED,
+                -1,
+                -1
+        );
+        Date start1 = TimestampConverter.getDateObject("2018-03-10 16:00:00");
+        Date end1 = TimestampConverter.getDateObject("2018-03-10 17:00:00"); //2x30mns
+        HashMap<Room, List<TimeSpan>> free_slots = this.optimisedSearch.search(this.token, 1, start1, end1, property);
+
+        Assert.assertEquals("Wrong # of rooms for the property.", 3, free_slots.size());
+        Room room2 = new Room(2, 1, 1, 1);
+        Room room5 = new Room(5, 2, 1, 4);
+        Room room6 = new Room(6, 2, 1, 4);
+        Assert.assertTrue("Room2 missing", free_slots.containsKey(room5));
+        Assert.assertTrue("Room5 missing", free_slots.containsKey(room5));
+        Assert.assertTrue("Room6 missing", free_slots.containsKey(room6));
+
+        //Room2
+        Assert.assertEquals(2, free_slots.get(room2).size()); //2
+        //Room5
+        Assert.assertEquals(2, free_slots.get(room5).size()); //2
+        //Room6
+        Assert.assertEquals(2, free_slots.get(room6).size()); //2
     }
 
+    /**
+     * Searches for available rooms on a floor within a time frame
+     */
     @Test
-    public void search6() throws Exception { //Searches for available rooms anywhere
+    public void search6() throws Exception {
+        RoomProperty property = new RoomProperty(
+                RoomProperty.Trilean.UNDEFINED,
+                RoomProperty.Trilean.UNDEFINED,
+                RoomProperty.Trilean.UNDEFINED,
+                RoomProperty.Trilean.UNDEFINED,
+                -1,
+                -1
+        );
+        Date start1 = TimestampConverter.getDateObject("2018-03-10 09:00:00");
+        Date end1 = TimestampConverter.getDateObject("2018-03-10 11:00:00"); //4x30mns
+        HashMap<Room, List<TimeSpan>> free_slots = this.optimisedSearch.search(this.token, 1, 1, start1, end1, property);
+
+        Assert.assertEquals("Wrong # of rooms for the property.", 3, free_slots.size());
+        Room room1 = new Room(1, 1, 1, 1);
+        Room room2 = new Room(2, 1, 1, 1);
+        Room room3 = new Room(3, 1, 1, 2);
+        Assert.assertTrue("Room1 missing", free_slots.containsKey(room1));
+        Assert.assertTrue("Room2 missing", free_slots.containsKey(room2));
+        Assert.assertTrue("Room3 missing", free_slots.containsKey(room3));
+
+        //Room1
+        Assert.assertEquals(2, free_slots.get(room1).size()); //4-(2)
+        //Room2
+        Assert.assertEquals(4, free_slots.get(room2).size()); //4-(0)
+        //Room3
+        Assert.assertEquals(2, free_slots.get(room3).size()); //4-(0)
+    }
+
+    /**
+     * Searches for available rooms anywhere
+     */
+    @Test
+    public void search7() throws Exception {
         RoomProperty property = new RoomProperty(
                 RoomProperty.Trilean.FALSE,
                 RoomProperty.Trilean.UNDEFINED,
@@ -107,23 +230,23 @@ public class OptimisedSearchTest {
         HashMap<Room, List<TimeSpan>> free_slots = this.optimisedSearch.search(this.token, start1, end1, property);
 
         Assert.assertEquals("Wrong # of rooms for the property.", 4, free_slots.size());
-        Room room1 = new Room(4, 2, 1, 3);
-        Room room2 = new Room(5, 2, 1, 4);
-        Room room3 = new Room(6, 2, 1, 4);
-        Room room4 = new Room(7, 3, 1, 5);
-        Assert.assertTrue("Room1 missing", free_slots.containsKey(room1));
-        Assert.assertTrue("Room2 missing", free_slots.containsKey(room2));
-        Assert.assertTrue("Room3 missing", free_slots.containsKey(room3));
+        Room room4 = new Room(4, 2, 1, 3);
+        Room room5 = new Room(5, 2, 1, 4);
+        Room room6 = new Room(6, 2, 1, 4);
+        Room room7 = new Room(7, 3, 1, 5);
         Assert.assertTrue("Room4 missing", free_slots.containsKey(room4));
+        Assert.assertTrue("Room5 missing", free_slots.containsKey(room5));
+        Assert.assertTrue("Room6 missing", free_slots.containsKey(room6));
+        Assert.assertTrue("Room7 missing", free_slots.containsKey(room7));
 
-        //Room1
-        Assert.assertEquals(36, free_slots.get(room1).size()); //48-(6+6)
-        //Room2
-        Assert.assertEquals(34, free_slots.get(room2).size()); //48-(2+2+3+3+4)
-        //Room3
-        Assert.assertEquals(48, free_slots.get(room3).size()); //48-(0)
         //Room4
-        Assert.assertEquals(40, free_slots.get(room4).size()); //48-(8)
+        Assert.assertEquals(36, free_slots.get(room4).size()); //48-(6+6)
+        //Room5
+        Assert.assertEquals(34, free_slots.get(room5).size()); //48-(2+2+3+3+4)
+        //Room6
+        Assert.assertEquals(48, free_slots.get(room6).size()); //48-(0)
+        //Room7
+        Assert.assertEquals(40, free_slots.get(room7).size()); //48-(8)
 
 //        for (Map.Entry<Room, List<TimeSpan>> entry : free_slots.entrySet()) {
 //            System.out.println("Room: " + entry.getKey());

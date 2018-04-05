a2e61d0eb2d51110ea39d7074c82db0d5969f093
An7ar35 <eadavison@protonmail.com>
2018-04-05 17:14:02 +0200

    [uaa] Implemented 'Authenticator'.

---
+++ services/src/main/java/bbk_beam/mtRooms/uaa/Authenticator.java
@@ -0,0 +1,31 @@
+package bbk_beam.mtRooms.uaa;
+
+import bbk_beam.mtRooms.ServiceDriver;
+import bbk_beam.mtRooms.admin.authentication.IAuthenticationSystem;
+import bbk_beam.mtRooms.admin.authentication.Token;
+import bbk_beam.mtRooms.admin.exception.AuthenticationFailureException;
+import bbk_beam.mtRooms.db.exception.SessionInvalidException;
+import bbk_beam.mtRooms.db.session.SessionType;
+
+public class Authenticator implements IAuthenticationSystem {
+    private IAuthenticationSystem authentication_system;
+
+    Authenticator(ServiceDriver driver) {
+        this.authentication_system = driver.getAuthenticationSystem();
+    }
+
+    @Override
+    public Token login(String username, String password) throws AuthenticationFailureException {
+        return this.authentication_system.login(username, password);
+    }
+
+    @Override
+    public void logout(Token session_token) throws SessionInvalidException {
+        this.authentication_system.logout(session_token);
+    }
+
+    @Override
+    public boolean hasValidAccessRights(Token session_token, SessionType user_session_type) {
+        return this.authentication_system.hasValidAccessRights(session_token, user_session_type);
+    }
+}

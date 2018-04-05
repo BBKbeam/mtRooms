b2244fe7d005c667b8834f438cb8a8cf7f0e5cc0
An7ar35 <eadavison@protonmail.com>
2018-04-05 13:28:57 +0200

    [operation.dto] Added 'OpsRoom' container class that extends 'Room' adding the capacity property to it; [operation.dto] Added 'LogisticsEntry' container class which serves as a line holder for the logistics report.

---
+++ services/src/main/java/bbk_beam/mtRooms/operation/dto/OpsRoom.java
@@ -0,0 +1,56 @@
+package bbk_beam.mtRooms.operation.dto;
+
+import bbk_beam.mtRooms.reservation.dto.Room;
+
+import java.util.Objects;
+
+public class OpsRoom extends Room {
+    private Integer capacity;
+
+    /**
+     * Constructor
+     *
+     * @param room_id     Room ID
+     * @param floor_id    Room's floor ID
+     * @param building_id Room's building ID
+     * @param category_id Room category ID
+     * @param capacity    Room capacity
+     */
+    public OpsRoom(Integer room_id, Integer floor_id, Integer building_id, Integer category_id, Integer capacity) {
+        super(room_id, floor_id, building_id, category_id);
+        this.capacity = capacity;
+    }
+
+    /**
+     * Gets the Room's capacity
+     *
+     * @return Max number of chairs
+     */
+    public Integer capacity() {
+        return this.capacity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        OpsRoom opsRoom = (OpsRoom) o;
+        return Objects.equals(capacity, opsRoom.capacity);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), capacity);
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.id() + "]={ "
+                + "floor_id: " + this.floorID()
+                + ", building_id: " + this.buildingID()
+                + ", category_id: " + this.category()
+                + ", capacity: " + this.capacity()
+                + " }";
+    }
+}
